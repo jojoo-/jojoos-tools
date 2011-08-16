3fcb7eb957e233a40a7e3da6c96c607d2d149149
Jonas Osswald <spam@fam-os.org>
2011-08-16 11:59:57 +0200

    ip-mac added

---
+++ ip_mac.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#script to output IPs on a mac.  Ethernet, airport and "External"
+
+myen0=`ifconfig en0 | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
+
+if [ "$myen0" != "" ]
+then
+    echo "Ethernet:  $myen0"
+else
+    echo "Ethernet:  INACTIVE"
+fi
+
+myen1=`ifconfig en1 | grep "inet " | grep -v 127.0.0.1 | awk '{print $2}'`
+if [ "myen1" != "" ]
+then
+    echo "AirPort :  $myen1"
+else
+    echo "Airport :  INACTIVE"
+fi
+echo -n "External:  "
+curl --silent http://checkip.dyndns.org | awk '{print $6}' |cut -f 1 -d "<"
